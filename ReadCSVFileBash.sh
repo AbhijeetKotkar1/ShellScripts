4be87f1b47b99c16dc87499d6b12f5e71ab4f850
Abhijeet Kotkar <k.abhi57@GMAIL.COM>
2019-05-26 19:10:15 +0530

    Add files via upload

---
+++ ReadCSVFileBash.sh
@@ -0,0 +1,15 @@
+#i/bin/bash
+
+input="/home/ninja/Documents/shellscripts/sample"
+
+while IFS= read -r line
+do
+
+	echo "Name : $(echo $line | sed 's/|/ /g' | awk '{print $1}')" &>> file.out
+	echo "Edu : $(echo $line | sed 's/|/ /g' | awk '{print $2}')" &>> file.out
+	echo "Address : $(echo $line | sed 's/|/ /g' | awk '{print $3}')" &>> file.out
+	echo "Salary : $(echo $line | sed 's/|/ /g' | awk '{print $4}')" &>> file.out
+
+done < "$input"
+
+#echo `cut -d, -f 1 $input | head -1`
